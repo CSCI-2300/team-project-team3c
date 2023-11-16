3ecb6f42ca78c60a08360fe9452fadb3fd7300a4
mkalal6 <137332780+mkalal6@users.noreply.github.com>
2023-11-15 22:01:50 -0600

    GameObserver methods

--- app/src/main/java/typespeed/view/GameObserver.java
+++ app/src/main/java/typespeed/view/GameObserver.java
@@ -2,4 +2,7 @@ package typespeed.view;
 
 public interface GameObserver{
     public void updateTimer(int time);
+    public void updateScore(int score); 
+    public void updateMissedWords(int missedWords);
+    void showScore(int score);
 }
