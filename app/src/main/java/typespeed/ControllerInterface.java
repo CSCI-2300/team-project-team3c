b9be2b6125bc7767d8a8e6902f26161e2e1597ba
mkalal6 <137332780+mkalal6@users.noreply.github.com>
2023-12-01 16:47:35 -0600

    deleted 2 files

--- app/src/main/java/typespeed/ControllerInterface.java
+++
@@ -1,9 +0,0 @@
-/*package typespeed;
-
-public interface ControllerInterface{
-    public void DisplayScore();
-    public void DisplayLevel();
-    public void DisplayTime();
-    public boolean isOver();
-
-}*/
